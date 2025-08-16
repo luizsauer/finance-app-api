44df4ca68185e639cf09ea84c4c3fa40be9811b2
luizsauer <luizsauer@gmail.com>
2025-08-15 23:29:38 -0300

    feat: add PostgresUpdateUserRepository

---
+++ src/db/postgres/repositories/postgres/update-user.js
@@ -0,0 +1,32 @@
+import { PostgresHelper } from '../../helper.js'
+
+export class PostgresUpdateUserRepository {
+    async execute(
+        userId,
+        updateUserParams = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+        },
+    ) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateUserParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateFields.length + 1}`)
+            updateValues.push(updateUserParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+            UPDATE users SET ${updateFields.join(', ')} WHERE id = $${updateFields.length + 1} RETURNING *
+        `
+        const updatedUser = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+        return updatedUser[0]
+    }
+}
