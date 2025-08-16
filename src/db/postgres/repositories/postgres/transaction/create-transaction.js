bad87a2e8dc27447d1819e1c6b80dec006330fbe
luizsauer <luizsauer@gmail.com>
2025-08-16 18:17:10 -0300

    feat: add user PostrgresCreateTransictionRepository

---
+++ src/db/postgres/repositories/postgres/transaction/create-transaction.js
@@ -0,0 +1,20 @@
+import { PostgresHelper } from '../../../helper.js' // Importing PostgresHelper for database operations
+
+export class PostgresCreateTransactionRepository {
+    async execute(createTransactionParams) {
+        const createdTransaction = await PostgresHelper.query(
+            `INSERT INTO transactions (id, user_id, name, date, amount, type) 
+            VALUES ($1, $2, $3, $4, $5, $6) 
+            RETURNING *`,
+            [
+                createTransactionParams.id,
+                createTransactionParams.user_id,
+                createTransactionParams.name,
+                createTransactionParams.date,
+                createTransactionParams.amount,
+                createTransactionParams.type,
+            ],
+        )
+        return createdTransaction[0] // retorna o primeiro campo da consulta
+    }
+}
