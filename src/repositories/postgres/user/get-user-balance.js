009058d54cff12be421534f1fa9985bafc0efe5b
luizsauer <luizsauer@gmail.com>
2025-08-18 15:52:41 -0300

    feat: ensure GetUserBalanceRepository uses Prisma

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,16 +1,66 @@
 // src\repositories\postgres\user\get-user-balance.js
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
-        const balance = await PostgresHelper.query(
-            `SELECT * FROM get_user_balance($1)`,
-            [userId],
-        )
+        const {
+            _sum: { amount: totalExpenseAmount },
+        } = await prisma.transaction.aggregate({
+            _sum: {
+                amount: true,
+            },
+            where: {
+                user_id: userId,
+                type: 'EXPENSE',
+            },
+        })
 
+        const {
+            _sum: { amount: totalEarningsAmount },
+        } = await prisma.transaction.aggregate({
+            _sum: {
+                amount: true,
+            },
+            where: {
+                user_id: userId,
+                type: 'EARNING',
+            },
+        })
+
+        const {
+            _sum: { amount: totalInvestmentsAmount },
+        } = await prisma.transaction.aggregate({
+            _sum: {
+                amount: true,
+            },
+            where: {
+                user_id: userId,
+                type: 'INVESTMENT',
+            },
+        })
+
+        const balance =
+            (totalEarningsAmount || 0) -
+            (totalExpenseAmount || 0) -
+            (totalInvestmentsAmount || 0)
+        console.log('balance', balance)
         return {
-            userId,
-            ...balance[0],
+            earnings: totalEarningsAmount,
+            expenses: totalExpenseAmount,
+            investments: totalInvestmentsAmount,
+            balance,
         }
     }
 }
+
+// async execute(userId) {
+//     const balance = await PostgresHelper.query(
+//         `SELECT * FROM get_user_balance($1)`,
+//         [userId],
+//     )
+
+//     return {
+//         userId,
+//         ...balance[0],
+//     }
+// }
--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,33 +1,44 @@
 // src\repositories\postgres\update-user.js
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateUserRepository {
-    async execute(
-        userId,
-        updateUserParams = {
-            first_name: '',
-            last_name: '',
-            email: '',
-            password: '',
-        },
-    ) {
-        const updateFields = []
-        const updateValues = []
-
-        Object.keys(updateUserParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateFields.length + 1}`)
-            updateValues.push(updateUserParams[key])
+    async execute(userId, updateUserParams) {
+        return await prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                ...updateUserParams,
+            },
         })
-
-        updateValues.push(userId)
-
-        const updateQuery = `
-            UPDATE users SET ${updateFields.join(', ')} WHERE id = $${updateFields.length + 1} RETURNING *
-        `
-        const updatedUser = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        )
-        return updatedUser[0]
     }
+
+    // async execute(
+    //     userId,
+    //     updateUserParams = {
+    //         first_name: '',
+    //         last_name: '',
+    //         email: '',
+    //         password: '',
+    //     },
+    // ) {
+    //     const updateFields = []
+    //     const updateValues = []
+
+    //     Object.keys(updateUserParams).forEach((key) => {
+    //         updateFields.push(`${key} = $${updateFields.length + 1}`)
+    //         updateValues.push(updateUserParams[key])
+    //     })
+
+    //     updateValues.push(userId)
+
+    //     const updateQuery = `
+    //         UPDATE users SET ${updateFields.join(', ')} WHERE id = $${updateFields.length + 1} RETURNING *
+    //     `
+    //     const updatedUser = await PostgresHelper.query(
+    //         updateQuery,
+    //         updateValues,
+    //     )
+    //     return updatedUser[0]
+    // }
 }
