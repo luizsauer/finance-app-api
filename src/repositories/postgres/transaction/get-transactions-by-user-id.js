8bd5f10361fd909e266b55529168e9d4184ea5cb
luizsauer <luizsauer@gmail.com>
2025-08-18 16:11:41 -0300

    feat: ensure Update and GetTransactionsByUserIdRepository uses Prisma

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -1,12 +1,25 @@
 // src\repositories\postgres\transaction\get-transactions-by-user-id.js
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetTransactionByUserIdRepository {
     async execute(userId) {
-        const transactions = await PostgresHelper.query(
-            'SELECT * FROM transactions WHERE user_id = $1',
-            [userId],
-        )
-        return transactions
+        try {
+            return await prisma.transaction.findMany({
+                where: {
+                    user_id: userId,
+                },
+            })
+        } catch (error) {
+            return null
+        }
     }
 }
+
+// async execute(userId) {
+//     const transactions = await PostgresHelper.query(
+//         'SELECT * FROM transactions WHERE user_id = $1',
+//         [userId],
+//     )
+//     return transactions
+// }
--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,27 +1,40 @@
 // src\repositories\postgres\transaction\update-transaction.js
-import { PostgresHelper } from '../../../db/postgres/helper.js' // Importing PostgresHelper for database operations
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        const updateFields = []
-        const updateValues = []
-
-        Object.keys(updateTransactionParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateFields.length + 1}`)
-            updateValues.push(updateTransactionParams[key])
-        })
-
-        updateValues.push(transactionId)
-
-        const updateQuery = `
-        UPDATE transactions
-        SET ${updateFields.join(', ')}
-        WHERE id = $${updateFields.length + 1}
-        RETURNING *
-    `
-
-        const updateUser = await PostgresHelper.query(updateQuery, updateValues)
-
-        return updateUser[0]
+        try {
+            return await prisma.transaction.update({
+                where: {
+                    id: transactionId,
+                },
+                data: updateTransactionParams,
+            })
+        } catch (error) {
+            return null
+        }
     }
 }
+
+// async execute(transactionId, updateTransactionParams) {
+//     const updateFields = []
+//     const updateValues = []
+
+//     Object.keys(updateTransactionParams).forEach((key) => {
+//         updateFields.push(`${key} = $${updateFields.length + 1}`)
+//         updateValues.push(updateTransactionParams[key])
+//     })
+
+//     updateValues.push(transactionId)
+
+//     const updateQuery = `
+//     UPDATE transactions
+//     SET ${updateFields.join(', ')}
+//     WHERE id = $${updateFields.length + 1}
+//     RETURNING *
+// `
+
+//     const updateUser = await PostgresHelper.query(updateQuery, updateValues)
+
+//     return updateUser[0]
+// }
