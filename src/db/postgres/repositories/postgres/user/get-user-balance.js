a67e2d72a035b0b54472c109bf9ecf8fefb6e298
luizsauer <luizsauer@gmail.com>
2025-08-18 01:07:25 -0300

    chore: add docker-compose file with postgres container

--- src/db/postgres/repositories/postgres/user/get-user-balance.js
+++ src/db/postgres/repositories/postgres/user/get-user-balance.js
@@ -3,18 +3,7 @@ import { PostgresHelper } from '../../../helper.js'
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
         const balance = await PostgresHelper.query(
-            `SELECT 
-                SUM(CASE WHEN TYPE = 'EARNING' THEN amount ELSE 0 END) AS earnings,
-                SUM(CASE WHEN TYPE = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
-                SUM(CASE WHEN TYPE = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
-
-                (
-                    SUM(CASE WHEN TYPE = 'EARNING' THEN amount ELSE 0 END)
-                    - SUM(CASE WHEN TYPE = 'EXPENSE' THEN amount ELSE 0 END)
-                    - SUM(CASE WHEN TYPE = 'INVESTMENT' THEN amount ELSE 0 END)
-                ) AS total
-            FROM transactions
-            WHERE user_id = $1`,
+            `SELECT * FROM get_user_balance($1)`,
             [userId],
         )
 
