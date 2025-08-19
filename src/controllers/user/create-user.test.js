cff360f2b7939ff95833a836dae3a86d58ea67ac
luizsauer <luizsauer@gmail.com>
2025-08-18 22:40:17 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -7,7 +7,7 @@ describe('Create User Controller', () => {
             return user
         }
     }
-    it('should create a user', async () => {
+    it('should return 201 when creating a user successfully', async () => {
         // Test implementation
         // Arrange
         const createUserUseCase = new CreateUserUseCaseStub()
@@ -28,5 +28,26 @@ describe('Create User Controller', () => {
         // Assert
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
+    })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // Test implementation
+        // Arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                last_name: 'Doe',
+                email: 'john@example.com',
+                password: 'password123',
+            },
+        }
+
+        // Act
+        const result = await createUserController.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(400)
     })
 })
