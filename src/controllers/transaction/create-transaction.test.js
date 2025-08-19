c04a6c69d1e4ebfa87cec3fc27398a98b16d5358
luizsauer <luizsauer@gmail.com>
2025-08-19 09:27:19 -0300

    test: ensure CreateTransactionController returns 400 when missing fields

---
+++ src/controllers/transaction/create-transaction.test.js
@@ -0,0 +1,179 @@
+import { faker } from '@faker-js/faker'
+import { CreateTransactionController } from './create-transaction.js'
+
+describe('CreateTransactionController', () => {
+    class CreateTransactionUseCaseStub {
+        async execute(transaction) {
+            return transaction
+        }
+    }
+
+    const makeSut = () => {
+        const createTransactionUseCase = new CreateTransactionUseCaseStub()
+        const sut = new CreateTransactionController(createTransactionUseCase)
+        return { sut, createTransactionUseCase }
+    }
+
+    const baseHttpRequest = {
+        body: {
+            user_id: faker.string.uuid(),
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: 'EXPENSE',
+            amount: Number(faker.finance.amount()),
+        },
+    }
+
+    // Test cases go here
+    it('should return 201 when creating transaction successfully (expense)', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute(baseHttpRequest)
+
+        expect(response.statusCode).toBe(201)
+    })
+
+    it('should return 201 when creating transaction successfully (earning)', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                type: 'EARNING',
+            },
+        })
+
+        expect(response.statusCode).toBe(201)
+    })
+
+    it('should return 201 when creating transaction successfully (investment)', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                type: 'INVESTMENT',
+            },
+        })
+
+        expect(response.statusCode).toBe(201)
+    })
+
+    it('should return 400 when missing user_id', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                user_id: undefined,
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when missing name', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                name: undefined,
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when missing date', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                date: undefined,
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when missing type', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                type: undefined,
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when missing amount', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                amount: undefined,
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when date is invalid', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                date: 'invalid-date',
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when amount is invalid', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                amount: 'invalid-amount',
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 when type is not EXPENSE, EARNING or INVESTMENT', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                type: 'INVALID_TYPE',
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 500 when CreateTransactionUseCase throws', async () => {
+        const { sut, createTransactionUseCase } = makeSut()
+
+        jest.spyOn(createTransactionUseCase, 'execute').mockRejectedValueOnce(
+            () => {
+                new Error()
+            },
+        )
+
+        const response = await sut.execute(baseHttpRequest)
+
+        expect(response.statusCode).toBe(500)
+    })
+})
---
+++ src/controllers/user/update-user.test.js
@@ -0,0 +1,191 @@
+import { faker } from '@faker-js/faker'
+import EmailAlreadyInUseError from '../../errors/user'
+import { UpdateUserController } from './update-user'
+
+describe('UpdateUserController', () => {
+    class UpdateUserUseCaseStub {
+        async execute(user) {
+            return user
+        }
+    }
+    const makeSut = () => {
+        const updateUserUseCase = new UpdateUserUseCaseStub()
+        const sut = new UpdateUserController(updateUserUseCase)
+
+        return { sut, updateUserUseCase }
+    }
+    const httpRequest = {
+        params: { userId: faker.string.uuid() },
+        body: {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({
+                length: 8,
+            }),
+        },
+    }
+
+    it('should return 200 if a user is updated successfully', async () => {
+        // Test implementation
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(200)
+    })
+
+    it('should return 400 if an invalid id is provided', async () => {
+        // Test implementation
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            params: { userId: 'invalid-id' },
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 when an invalid email is provided', async () => {
+        // Test implementation
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            params: { userId: faker.string.uuid() },
+            body: {
+                email: 'invalid-email',
+            },
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 when an invalid password is provided', async () => {
+        // Test implementation
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            params: { userId: faker.string.uuid() },
+            body: {
+                password: faker.internet.password({
+                    length: 5,
+                }),
+            },
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 when an invalid id is provided', async () => {
+        // Test implementation
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            params: { userId: 'invalid_id' },
+            body: httpRequest.body,
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 when an unallowed field is provided', async () => {
+        // Test implementation
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest.body,
+                unallowed_field: faker.lorem.sentence(),
+            },
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 500 if UpdateUserUseCase throws with generic error', async () => {
+        // Test implementation
+        // Arrange
+        const { sut, updateUserUseCase } = makeSut()
+        jest.spyOn(updateUserUseCase, 'execute').mockRejectedValue(
+            new Error('Internal Server Error'),
+        )
+
+        // Act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: httpRequest.body,
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(500)
+    })
+
+    it('should return 400 if UpdateUserUseCase throws EmailAlreadyInUseError', async () => {
+        // Test implementation
+        // Arrange
+        const { sut, updateUserUseCase } = makeSut()
+        jest.spyOn(updateUserUseCase, 'execute').mockRejectedValueOnce(
+            new EmailAlreadyInUseError(faker.internet.email()),
+        )
+
+        // Act
+        const result = await sut.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    // it('should return 404 if no user is found', async () => {
+    //     // Test implementation
+    //     // Arrange
+    //     const { sut, updateUserUseCase } = makeSut()
+    //     jest.spyOn(updateUserUseCase, 'execute').mockResolvedValue(null)
+
+    //     // Act
+    //     const result = await sut.execute({
+    //         params: { userId: faker.string.uuid() },
+    //     })
+
+    //     // Assert
+    //     expect(result.statusCode).toBe(404)
+    // })
+
+    // it('should return 500 if UpdateUserUseCase throws an error', async () => {
+    //     // Test implementation
+    //     // Arrange
+    //     const { sut, updateUserUseCase } = makeSut()
+    //     jest.spyOn(updateUserUseCase, 'execute').mockRejectedValue(
+    //         new Error('Internal Server Error'),
+    //     )
+
+    //     // Act
+    //     const result = await sut.execute({
+    //         params: { userId: faker.string.uuid() },
+    //     })
+
+    //     // Assert
+    //     expect(result.statusCode).toBe(500)
+    //     // expect(result.body).toEqual({
+    //     //     error: 'Internal Server Error',
+    //     // })
+    // })
+})
