861ebf1956aa2a8b56a86b898c82181b9737e38d
luizsauer <luizsauer@gmail.com>
2025-08-20 13:49:44 -0300

    test: ensure PostgresUpdateTransactionRepository should call Prisma with correct params

---
+++ src/repositories/postgres/transaction/update-transaction.test.js
@@ -0,0 +1,49 @@
+import { faker } from '@faker-js/faker'
+import { TransactionType } from '@prisma/client'
+import dayjs from 'dayjs'
+import { prisma } from '../../../../prisma/prisma'
+import { transaction, user } from '../../../tests'
+import { PostgresUpdateTransactionRepository } from './update-transaction.js'
+
+describe('PostgresUpdateTransactionRepository', () => {
+    it('should update a transaction on db', async () => {
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({
+            data: { ...transaction, user_id: user.id },
+        })
+        const sut = new PostgresUpdateTransactionRepository()
+
+        const params = {
+            id: faker.string.uuid(),
+            user_id: user.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.EXPENSE,
+            amount: Number(faker.finance.amount()),
+        }
+
+        const result = await sut.execute(transaction.id, params)
+
+        expect(result.name).toBe(params.name)
+        expect(result.type).toBe(params.type)
+        expect(result.user_id).toBe(user.id)
+        expect(String(result.amount)).toBe(String(params.amount))
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(params.date).daysInMonth(),
+        )
+        expect(dayjs(result.date).month()).toBe(dayjs(params.date).month())
+        expect(dayjs(result.date).year()).toBe(dayjs(params.date).year())
+    })
+
+    it('should call Prisma with correct params', async () => {})
+
+    it('should throw if Prisma throws', async () => {})
+
+    // it('should return null if user has no transactions', async () => {
+
+    // })
+
+    // it('should return null if transaction does not exist', async () => {
+
+    // })
+})
