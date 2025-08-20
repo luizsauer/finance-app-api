95cd75b9121e0e3103c0f31066119c9fbe92b838
luizsauer <luizsauer@gmail.com>
2025-08-20 11:21:02 -0300

    test: ensure PostgresGetUserById should call Prisma with correct params

---
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -0,0 +1,35 @@
+import { prisma } from '../../../../prisma/prisma'
+import { user as fakerUser } from '../../../tests'
+import { PostgresGetUserByIdRepository } from './get-user-by-id.js'
+
+describe('PostgresGetUserByIdRepository', () => {
+    it('should return user if id exists', async () => {
+        const user = await prisma.user.create({ data: fakerUser })
+
+        const sut = new PostgresGetUserByIdRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toEqual(user)
+    })
+
+    // it('should return null if id does not exist', async () => {
+    //     const sut = new PostgresGetUserByIdRepository()
+
+    //     const result = await sut.execute(faker.datatype.uuid())
+
+    //     expect(result).toBeNull()
+    // })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserByIdRepository()
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+
+        await sut.execute(fakerUser.id)
+
+        expect(prismaSpy).toHaveBeenCalledTimes(1)
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: { id: fakerUser.id },
+        })
+    })
+})
