c9615f8e16e65ff4aa390186121925779b4f9c0a
luizsauer <luizsauer@gmail.com>
2025-08-20 16:27:45 -0300

    chore: ajust in jest.setup-after for disconnect prisma after tests

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -4,3 +4,7 @@ beforeEach(async () => {
     await prisma.transaction.deleteMany()
     await prisma.user.deleteMany()
 })
+
+afterAll(async () => {
+    await prisma.$disconnect() // fecha as conexões no final
+})
