8f04e8c9d3221de0aa668702a3c362e5adb4043b
luizsauer <luizsauer@gmail.com>
2025-08-20 03:46:37 -0300

    chore: add jest global setup file to run docker before all tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,6 +5,9 @@ const config = {
     coverageProvider: 'v8',
     collectCoverageFrom: ['src/**/*.js'],
     coveragePathIgnorePatterns: ['/src/generated/prisma/'],
+    globalSetup: '<rootDir>/jest.global-setup.mjs',
+    // globalTeardown: '<rootDir>/jest.global-teardown.js',
+    // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
 
 export default config
---
+++ jest.global-setup.mjs
@@ -0,0 +1,10 @@
+import { execSync } from 'child_process'
+
+async function init() {
+    // Global setup logic
+    execSync('docker-compose up -d --wait postgres-test')
+    execSync('npx prisma db push')
+    // execSync('npm run migrate:latest', { stdio: 'inherit' })
+}
+
+export default init
