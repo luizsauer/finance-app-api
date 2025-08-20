0d129fb11a283287395985b69adadd0a2a3d54ae
luizsauer <luizsauer@gmail.com>
2025-08-19 23:08:14 -0300

    test: ensure GetUserByIdUseCase throws if GetUserByIdRepository throws

--- src/use-cases/user/get-user-by-id.test.js
+++ src/use-cases/user/get-user-by-id.test.js
@@ -35,4 +35,36 @@ describe('GetUserByIdUseCase', () => {
 
         expect(result).toEqual(user)
     })
+
+    it('should call getUserByIdRepository with correct params', async () => {
+        const { sut, getUserByIdRepository } = makeSut()
+        const userId = faker.string.uuid()
+        const executeSpy = jest.spyOn(getUserByIdRepository, 'execute')
+
+        await sut.execute(userId)
+
+        expect(executeSpy).toHaveBeenCalledWith(userId)
+    })
+
+    it('should throw if getUserByIdRepository throws', async () => {
+        const { sut, getUserByIdRepository } = makeSut()
+
+        jest.spyOn(getUserByIdRepository, 'execute').mockRejectedValue(
+            new Error(),
+        )
+
+        const promise = sut.execute(faker.string.uuid())
+
+        await expect(promise).rejects.toThrow()
+    })
+
+    it('should return null if user is not found', async () => {
+        const { sut, getUserByIdRepository } = makeSut()
+
+        jest.spyOn(getUserByIdRepository, 'execute').mockResolvedValueOnce(null)
+
+        const result = await sut.execute(faker.string.uuid())
+
+        expect(result).toBeNull()
+    })
 })
